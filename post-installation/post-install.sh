faba0c60c384a6eb8c4cae65e23849a262e625f3
Lukas <lukas@phyllo.me>
2021-11-05 15:33:43 +0100

    correct path for virsh pool

--- post-installation/post-install.sh
+++ post-installation/post-install.sh
@@ -2,7 +2,7 @@
 # It is then executed once upon first-boot as a systemd-unit
 
 # virsh command fail. would need to fetch a script and execute post-launch with a delay, for example using a systemd unit 
-virsh pool-define-as iso dir /var/lib/libvirt/iso/ # Make libvirt aware of this new directory by creating a so-called 'pool'.
+virsh pool-define-as iso dir - - - - /var/lib/libvirt/iso/ # Make libvirt aware of this new directory by creating a so-called 'pool'.
 virsh pool-build iso # Build the pool
 virsh pool-start iso # Start it
 virsh pool-autostart iso # Set-it to autostart
