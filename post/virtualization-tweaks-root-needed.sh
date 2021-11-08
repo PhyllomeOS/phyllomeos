bda92766081711e3230478c3d8ee9c8c02239ef4
Lukas <lukas@phyllo.me>
2021-11-08 17:08:02 +0100

    virtualization-related tweaks

---
+++ post/virtualization-tweaks-root-needed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+usermod -a -G libvirt $(whoami) # add current user to the libvirt group
+
+# virsh commands fail in a kickstart environment (chroot or not it seems). would need to fetch a script and execute post-launch with a delay, for example using a systemd unit 
+virsh pool-define-as iso dir - - - - /var/lib/libvirt/iso/ # Make libvirt aware of this new directory by creating a so-called 'pool'.
+virsh pool-build iso # Build the pool
+virsh pool-start iso # Start it
+virsh pool-autostart iso # Set-it to autostart
