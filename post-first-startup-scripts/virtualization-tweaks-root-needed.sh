f9822a5b96991a2560e30bad7f770c3a648264af
lukas <lukas.phyllo.me>
2023-05-29 20:56:17 +0200

    fixes
    remove deprecated background images
    define /var/lib/libvirt/isos to store isos
    correct isos paths

--- post-first-startup-scripts/virtualization-tweaks-root-needed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# usermod -a -G libvirt kvm $(whoami) # add current user to the libvirt and kvm groups
-
-# virsh commands fail in a kickstart environment (chroot or not it seems). would need to fetch a script and execute post-launch with a delay, for example using a systemd unit 
-virsh pool-define-as iso dir - - - - /var/lib/libvirt/iso/ # Make libvirt aware of this new directory by creating a so-called 'pool'.
-virsh pool-build iso # Build the pool
-virsh pool-start iso # Start it
-virsh pool-autostart iso # Set-it to autostart
