63456e0e160a8c3434c05b9ab5901ac845a32d28
Lukas Greve <please@refre.ch>
2025-05-22 18:10:43 +0200

    bump hardware requirements

--- scripts/core-count.sh
+++ scripts/core-count.sh
@@ -4,10 +4,10 @@
 core_count=$(nproc --all)
 
 # Check if nproc --all returns a numerical value greater than 2
-if (( core_count > 1 )); then
- echo "System has more than 1 core (nproc --all: $core_count)."
+if (( core_count > 2 )); then
+ echo "System has more than 2 core (nproc --all: $core_count)."
 else
   echo "Warning: System has only $core_count core)."
-  echo "The script requires at least two cores"
+  echo "The script requires at least four cores"
   exit 1  # Exit with an error code to indicate the condition is not met
 fi
--- scripts/system-memory.sh
+++ scripts/system-memory.sh
@@ -6,8 +6,8 @@ total_memory=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
 # Convert to MiB by dividing by 1024 (since MemTotal is in KiB)
 total_memory_mb=$(( total_memory / 1024 ))
 
-if [[ "$total_memory_mb" -lt "2048" ]]; then
-    echo "Not enough RAM: The system has only ${total_memory_mb}MiB of RAM, but at least 2048 MiB is required."
+if [[ "$total_memory_mb" -lt "4096" ]]; then
+    echo "Not enough RAM: The system has only ${total_memory_mb}MiB of RAM, but at least 4096 is required."
     exit 1
 else
     echo "Sufficient memory available. System has ${total_memory_mb}MiB of RAM."
