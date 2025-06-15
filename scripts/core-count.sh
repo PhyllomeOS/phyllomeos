684a49af14365265d11b3214314e2bfb760e26eb
Lukas Greve <please@refre.ch>
2025-06-15 14:30:00 +0200

    no need to echo when it succeeds

--- scripts/core-count.sh
+++ scripts/core-count.sh
@@ -4,9 +4,7 @@
 core_count=$(nproc --all)
 
 # Check if nproc --all returns a numerical value greater than 2
-if (( core_count > 2 )); then
- echo "System has more than 2 core (nproc --all: $core_count)."
-else
+if (( core_count < 2 )); then
   echo "Warning: System has only $core_count core)."
   echo "The script requires at least four cores"
   exit 1  # Exit with an error code to indicate the condition is not met
